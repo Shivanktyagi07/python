f2efe661e3dba065dd5d775c7110d8d6a46b4982
Shivanktyagi07 <shivanktyagi976@gmail.com>
2024-01-30 21:29:30 +0530

    second commit

---
+++ 02_conditional/01_if Else/first.py
@@ -0,0 +1,7 @@
+number = int (input("Enter any number"))
+if number > 0:
+    print(str(number) + " " +"is positive.")
+if number ==0:
+    print(str(number) + "is zero")
+if number <0:
+    print( str(number) + "is negative")
